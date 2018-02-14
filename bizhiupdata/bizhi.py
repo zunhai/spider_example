acb1e879c77ec9565aa05dbe853344e6e8c8c9e3
zunhai <18595456925@163.com>
2018-02-14 11:42:36 +0800

    解析json数据

--- bizhiupdata/bizhi.py
+++ bizhiupdata/bizhi.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
      headers = {'authorization':'Client-ID c94869b36aa272dd62dfaeefed769d4115fb3189a9d1ec88ed457207747be626'}
      req = requests.get(url=target, headers=headers, verify=False)
      html = json.loads(req.text)
+    
      next_page = html['next_page']
      print('下一页地址:',next_page)
      for each in html['photos']:
