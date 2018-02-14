23cb182d9daa093a43ac0e491cf64c8e013b7909
zunhai <18595456925@163.com>
2018-02-14 11:35:11 +0800

    获取json数据

--- bizhiupdata/bizhi.py
+++ bizhiupdata/bizhi.py
@@ -1,7 +1,7 @@
-import requests
-
+import requests, json
 if __name__ == '__main__':
-    target = 'https://unsplash.com/napi/feeds/home'
+     target = 'http://unsplash.com/napi/feeds/home'
+     headers = {'authorization':'Client-ID c94869b36aa272dd62dfaeefed769d4115fb3189a9d1ec88ed457207747be626'}
+     req = requests.get(url=target, headers=headers, verify=False)
 
-    req = requests.get(url=target, verify=False)
-    print(req.text)
+     print(req.text)
