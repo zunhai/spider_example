467bc9054364961a16756b02b5a90a68863f6dd3
zunhai <18595456925@163.com>
2018-02-14 11:36:17 +0800

    获取图片id

--- bizhiupdata/bizhi.py
+++ bizhiupdata/bizhi.py
@@ -3,5 +3,8 @@ if __name__ == '__main__':
      target = 'http://unsplash.com/napi/feeds/home'
      headers = {'authorization':'Client-ID c94869b36aa272dd62dfaeefed769d4115fb3189a9d1ec88ed457207747be626'}
      req = requests.get(url=target, headers=headers, verify=False)
-
-     print(req.text)
+     html = json.loads(req.text)
+     next_page = html['next_page']
+     print('下一页地址:',next_page)
+     for each in html['photos']:
+          print('图片ID:',each['id'])
